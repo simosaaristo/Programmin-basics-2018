07e6d579b3e2c34a7e6937b858260bcd8d93bf37
simosaaristo <simo_saaristo@hotmail.com>
2018-09-19 09:06:29 +0300

    condition tasks added

---
+++ condition tasks/IfTask1/IfTask1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IfTask1
+{
+    //This one line comment
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program solves if the number is positive, negative or zero.");
+            Console.WriteLine("")
+        }
+    }
+}
