a4b0f0db9abe6a9ccaa1f6c8a32512bda32e3711
simosaaristo <simo_saaristo@hotmail.com>
2018-09-27 13:47:58 +0300

    IfTask4 done

---
+++ condition tasks/IF-tehtävät/IfTask4/IfTask4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace IfTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma lajittelee luvut nousevaan järjestykseen.");
+            Console.Write("Syötä 1.luku: ");
+            string userInput = Console.ReadLine();
+            int a = int.Parse(userInput);
+            Console.Write("Syötä 2. luku: ");
+            userInput = Console.ReadLine();
+            int b = int.Parse(userInput);
+            Console.Write("Syötä 3. luku: ");
+            userInput = Console.ReadLine();
+            int c = int.Parse(userInput);
+
+            if (a > b)
+            {
+                if (b > c)
+                    Console.Write($"{c}, {b} ja {a} ");
+                else
+                {
+                    if (c > a)
+                        Console.Write($"{b}, {a} ja {c} ");
+                    else
+                        Console.Write($"{b}, {c} ja {a} ");
+                }
+            }
+            else
+            {
+                if (a > c)
+                    Console.Write($"{c}, {a} ja {b} ");
+                else
+                {
+                    if (b > c)
+                        Console.Write($"{a}, {c} ja {b} ");
+                    else
+                        Console.Write($"{a}, {b} ja {c} ");
+
+                }
+
+            }
+           
+
+                  
+
+
+
+        }
+    }
+}
